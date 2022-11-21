ef30997c17d0fd4613e61b4c5a50d33adb98ddef
searcheuchheang <searcheuchheang@gmail.com>
2022-11-21 21:10:49 +0700

    Initial Commit

---
+++ randomPhotoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  randomPhotoApp
+//
+//  Created by Macintosh on 21/11/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ randomPhotoApp/randomPhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomPhotoAppApp.swift
+//  randomPhotoApp
+//
+//  Created by Macintosh on 21/11/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct randomPhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
