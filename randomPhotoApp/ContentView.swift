051d685bdb084bd2ac82d721224814a3ad4cdfbd
searcheuchheang <searcheuchheang@gmail.com>
2022-11-21 23:40:43 +0700

    random photo app

--- randomPhotoApp/ContentView.swift
+++ randomPhotoApp/ContentView.swift
@@ -7,10 +7,59 @@
 
 import SwiftUI
 
+class ViewModal: ObservableObject {
+    @Published var image: Image?
+    
+    func fetchNewImage() {
+        guard let url = URL(string:"https://random.imagecdn.app/500/500") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else {return}
+            DispatchQueue.main.async {
+                guard let uiImage = UIImage(data: data) else {return}
+                self.image = Image(uiImage:uiImage)
+            }
+        }
+        task.resume()
+    }
+}
+
+
 struct ContentView: View {
+   @StateObject var viewModal = ViewModal()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Spacer()
+                if let image = viewModal.image{
+                        image
+                            .resizable()
+                            .foregroundColor(Color.pink)
+                            .frame(width: 200, height: 200)
+                            .padding()
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .foregroundColor(Color.pink)
+                        .frame(width: 200, height: 200)
+                        .padding()
+                }
+                Spacer()
+                Button(action: {
+                    viewModal.fetchNewImage()
+                }, label: {
+                    Text("New image!")
+                        .bold()
+                        .foregroundColor(Color.white)
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(20)
+                        .padding()
+                })
+            }
+            .navigationTitle("Photo Random")
+        }
     }
 }
 
